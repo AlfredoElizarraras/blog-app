9ec54b0432d834ccb1f2d846398283dcd3574070
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-04-15 12:30:20 -0500

    Add deleting and creating articles actions.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,32 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash.notice = "Article '#{@article.title}' Created!"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |indx|
+  a = Article.new
+  a.title = "title #{indx}"
+  a.text = "text #{indx}"
+  a.save
+end
