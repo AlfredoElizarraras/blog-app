143533698456b86e4d5d1928efe160620505bd93
flakster <carlos.santamaria@gmail.com>
2020-04-16 12:58:40 -0500

    Add Tags to the blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -45,7 +45,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :text, :tag_list)
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,20 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
+
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")  
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags 
+  end
+  
 end
